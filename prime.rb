9b5dc11c68561cf6468c9a27e3b94989218a0fd5
Vytautas Klimavicius <vyklimavicius@gmail.com>
2019-01-31 17:37:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(n)
     if n <= 1 
       return false 
     end 
-   is_prime = true
+    is_prime = true
     for i in 2..n-1 # For each number that we have from 2 to n-1 
       if n % i == 0
         is_prime = false
