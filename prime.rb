9983fbfde0da76dbb6399bbfda03f74e78a9bb2a
Vytautas Klimavicius <vyklimavicius@gmail.com>
2019-01-31 17:35:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,13 @@
 def prime?(n)
-   
+   is_prime = true
+    for i in 2..n-1 # For each number that we have from 2 to n-1 
+      if n % i == 0
+        is_prime = false
+      end
+    end
+    if is_prime
+      true
+    else
+      false
+    end
 end 
